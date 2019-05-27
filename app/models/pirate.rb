1f0e7f00945e85a6bca70ea5bd757086b216d890
Mickae Truman <mtruman@nyit.edu>
2019-05-27 22:35:37 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -6,5 +6,11 @@ class Pirate
   
   def initialize(params)
     @name = params[:name]
-    @weight = p
+    @weight = params[:weight]
+    @height = params[:height]
+  end
+  
+  def self.all
+    @@all
+  end
 end
